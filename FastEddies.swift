f560e53043bfaf81293388251bf4e2f9221bc2e3
Rob Evans <robevans@microsoft.com>
2020-08-24 14:33:41 -0400

    Update FastEddies.swift
    redesign

--- FastEddies.swift
+++ FastEddies.swift
@@ -13,42 +13,57 @@ struct FastEddies: View {
     @State var show = false
     @State var viewState = CGSize.zero //storing a value of CGSize with xy position with value 0
     @State var showCard = false
+    @State var isDragging = false
     //listens to changes in stopwatchmanager class
 
 
 
     var body: some View {
-        VStack (spacing: 16){
-            VStack(spacing: 16) {
+        ZStack{
+            Color("background3")
+                .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            VStack {
+                GeometryReader { geometry in
+                    HStack {
+                        Text("Fast Eddies")
+                            .font(.system(size: geometry.size.width/10, weight: .bold))
+                            .foregroundColor(Color("background1"))
+                        Spacer()
+                        Image("Weida")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 65, height: 65)
+                            .clipShape(Circle())
+                    }
+                }
+                    .frame(maxWidth: 375, maxHeight: 100) //375 (geometry.size.width/10) - 32(padding) = 343(screen size)
+                    .padding(.horizontal, 16)
+                    .offset(x: viewState.width/15, y: viewState.height/15)
                 Text("• 25 Situps \n• 25 fluter kicks - 4 count\n• 25 Situps\n• 25 Leg Raised\n• 25 Situps\n• 25 Holly Dolly's - 4 count\n• 25 Situps\n• 25 Crunches\n• 25 Situps\n• 25 Crazy Ivan's\n• 25 Situps\n• 25 Bicycleers - 4 count")
-                    .padding(.top, 30)
-                    .foregroundColor(Color("background1"))
-                Text("Sub 10 is good").font(.caption)
-                    .foregroundColor(Color("background1"))
+                Spacer()
+                Text("10 Minutes is ok\n 8 minutes is good")
+                    .font(.caption)
             }
+            .multilineTextAlignment(.center)
+            .padding(.top, 10)
+            .padding(.bottom, 10)
+            .frame(height: 477)
             .frame(maxWidth: .infinity)
-            .frame(height: 300)
-            .background(Color("card6"))
-            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(color: Color("card6").opacity(0.4), radius: 10, x: 0, y: 20)
-            .padding(.horizontal, 30)
-            .overlay(
-                Image("Weida")
-                    .resizable()
-//                    .aspectRatio(contentMode: .fill)
-            .frame(width: 75, height: 75)
-                    .clipShape(Circle())
-                    .offset(y: -150)
-            )
-                //Begins drag effect
-                .offset(x: viewState.width, y: viewState.height)
-                .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0.6))
+            .background(Color (#colorLiteral(red: 0.5741485357, green: 0.5741624236, blue: 0.574154973, alpha: 1))
+            .offset(x: viewState.width/25, y: viewState.height/25)
+                , alignment: .bottom)
+                .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                .scaleEffect(isDragging ? 0.9 : 1)
+                .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+                .rotation3DEffect(Angle(degrees: 7), axis: (x: viewState.width, y: viewState.height, z: 0))
                 .gesture(
                     DragGesture().onChanged { value in
                         self.viewState = value.translation
+                        self.isDragging = true
                     }
-                    .onEnded(){value in
+                    .onEnded { value in
                         self.viewState = .zero
+                        self.isDragging = false
                     }
             )
         }
@@ -57,22 +72,7 @@ struct FastEddies: View {
 
 struct FastEddies_Previews: PreviewProvider {
     static var previews: some View {
-        FastEddies()
-//        FastEddies().environment(\.colorScheme, .dark)
-    }
-}
-
-struct TimerButton: View {
-
-    let label: String
-    let buttonColor: Color
-
-    var body: some View {
-        Text(label)
-            .foregroundColor(.white)
-//            .padding(.vertical, 20)
-//            .padding(.horizontal, 90)
-            .background(buttonColor)
-            .cornerRadius(10)
+        //        FastEddies()
+        FastEddies().environment(\.colorScheme, .dark)
     }
 }
