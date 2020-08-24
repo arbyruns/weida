f00d737b259c651bb4ec2348b64cbef1633e2bc7
Rob Evans <robevans@microsoft.com>
2020-08-24 14:35:35 -0400

    Update Favorites.swift

--- Favorites.swift
+++ Favorites.swift
@@ -12,4 +12,10 @@ import Combine
 final class UserData: ObservableObject  {
     @Published var showFavoritesOnly = false
     @Published var week = weeks //weeks is the array object data where var is variable
+
+    init() {
+        UserDefaults.standard.data(forKey: "FavStatus")
+        print("Loading week data: \(self.week)")
+    }
+
 }
