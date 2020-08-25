3ff1f29f12de28fdfb37427887c9358dc763d84e
Rob Evans <robevans@microsoft.com>
2020-08-25 16:00:39 -0400

    checking a ton of changes into dev

--- SplashView.swift
+++ SplashView.swift
@@ -25,7 +25,6 @@ struct SplashView: View {
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-//        .background(RadialGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.1298420429, green: 0.1298461258, blue: 0.1298439503, alpha: 1)), Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, startRadius: 20, endRadius: 600))
         .background(Color(#colorLiteral(red: 0.2605174184, green: 0.2605243921, blue: 0.260520637, alpha: 1)))
         .animation(.easeInOut)
         .onAppear {
